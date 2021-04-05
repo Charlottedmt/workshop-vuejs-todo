526428ab63afdc65eb6b0ba33317d9673b249291
Charlotte Demaret <demaret.cha@gmail.com>
2021-04-05 21:08:53 +0900

    Start new project from /trouni/workshop-vuejs-tod

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" height="100" />
+    <h1>Task Manager</h1>
+    <TasksList></TasksList>
+    <p>Let's build our first Vue.js app!</p>
+  </div>
+</template>
+
+<script>
+import TasksList from './components/TasksList.vue'
+export default {
+  name: "App",
+  components: {
+    TasksList
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./assets/style.scss";
+</style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="tasks">
+    <button
+      class="btn round-icon"
+      v-on:click="addTask('My new task', 'My new description'), resetForm()"
+      @click="addTask(newTitle, newDescription)">+</button>
+
+    <div class="task-card new-task">
+      <div>
+        <input
+          type="text"
+          placeholder="What would you like to do?"
+          v-model="newTitle"
+        />
+        <textarea
+      placeholder="Add some details about your task..."
+      v-model="newDescription"
+        ></textarea>
+      </div>
+    </div>
+    <div v-if="tasks.length > 0">
+      <TaskCard
+        v-for="(task, index) in tasks"
+        :key="index"
+        :title="task.title"
+        :description="task.description"
+        :done="task.done">
+      </TaskCard>
+    </div>
+    <p v-else>You don't have any tasks yet 😢</p>
+  </div>
+</template>
+<script>
+import TaskCard from './TaskCard'
+export default {
+  components: { TaskCard },
+  data() {
+    return {
+      tasks: [],
+
+    };
+  },
+  methods: {
+    addTask(title, description, done = false) {
+      this.tasks.unshift({ title, description, done});
+    },
+    resetForm() {
+      this.newTitle= "";
+      this.newDescription = "";
+    }
+  },
+}
+</script>
